4da3442b59b60950115db18dfa22935f873a769f
smmsadrnezh <smmsadrnezh@gmail.com>
2014-01-21 19:52:24 +0330

    cleaning

--- src/com/cebomberman/game/graphics/GameStarterComponents.java
+++ src/com/cebomberman/game/graphics/GameStarterComponents.java
@@ -1,11 +1,5 @@
 package com.cebomberman.game.graphics;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * 
  * @author sadrnezhaad
